f09b845d7608dccc7d5afb6ba00392f748d7395e
Grigorich <oskinnick@gmail.com>
2022-09-23 12:58:57 +0300

    Initial commit

---
+++ src/main/java/org/mykola/springDataJdbcStudy/SpringDataJdbcStudyApplication.java
@@ -0,0 +1,13 @@
+package org.mykola.springDataJdbcStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJdbcStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJdbcStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mykola/springDataJdbcStudy/SpringDataJdbcStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mykola.springDataJdbcStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJdbcStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
